eb1fdbbfa7ee3f8d8edbeed0e2723318be7f1611
Adam Kajtar <akajtar@wadsworthcity.org>
2019-10-03 07:46:57 -0700

    test

---
+++ ex/ex2/ex2_1.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+
+CISCO4 = {
+    'host': 'cisco4.lasthop.io',
+    'username': 'pyclass',
+    'password': '88newclass',
+    'device_type': 'cisco_ios',
+
+}
+
+net_connect = ConnectHandler(**CISCO4)
+
